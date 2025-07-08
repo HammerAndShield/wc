a2877c92b47753cf66a02e9942fbb2448b09b130
Austin Townsend <130945920+HammerAndShield@users.noreply.github.com>
2025-07-07 20:56:50 -0700

    add swift ArguementParser package

--- swift/Package.swift
+++ swift/Package.swift
@@ -5,10 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "wc",
+    platforms: [.macOS(.v14)],
+    products: [
+        .executable(name: "wc", targets: ["wc"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", 
+                 from: "1.6.1")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "wc"),
+            name: "wc",
+            dependencies: [
+                .product(name: "ArgumentParser",
+                         package: "swift-argument-parser"),
+            ]),
     ]
 )
---
+++ swift/Sources/wc/main.swift
@@ -0,0 +1,10 @@
+import ArgumentParser
+
+@main 
+struct Main: ParsableCommand {
+    
+
+    mutating func run() {
+        print("Hello, World!")
+    }
+}
