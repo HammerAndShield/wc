1663b057efed77fb9ee93336931313dbfda285f6
Austin Townsend <130945920+HammerAndShield@users.noreply.github.com>
2025-07-07 22:44:36 -0700

    finish swift c flag implementation

--- swift/Sources/wc/main.swift
+++ swift/Sources/wc/main.swift
@@ -1,10 +1,47 @@
 import ArgumentParser
+import Foundation
 
 @main 
-struct Main: ParsableCommand {
-    
+struct Main: AsyncParsableCommand {
+    @Flag(name: .customShort("c"), help: "Read the byte count of the file.")
+    var countBytes: Bool = false
 
-    mutating func run() {
-        print("Hello, World!")
+    @Argument(
+        help: "File to be read",
+        transform: { URL(filePath: $0) } 
+    )
+    var fileName: URL
+
+    mutating func run() async {
+        if countBytes {
+            do {
+                let start = Date()
+                let fileHandle = try FileHandle(forReadingFrom: fileName)
+                defer { try? fileHandle.close() }
+
+                let chunkSize = 4096
+                var totalBytes = 0
+                while true {
+                    let chunk = try fileHandle.read(upToCount: chunkSize)
+                    guard let chunk = chunk else {
+                        break
+                    }
+                    totalBytes += chunk.count
+                }
+
+                // var totalBytes = 0
+                // for try await _ in fileHandle.bytes {
+                //     totalBytes += 1
+                // }
+
+                let processingTime = Date().timeIntervalSince(start)
+                print("\(totalBytes) \(fileName.lastPathComponent)")
+                print("processing time: \(processingTime * 1000) ms")
+            } catch {
+                fatalError("There was an error processing your file: \(error.localizedDescription)")
+            }
+        } else {
+            print("You must specify a command line flag. Check -h for help.")
+        }
     }
 }
